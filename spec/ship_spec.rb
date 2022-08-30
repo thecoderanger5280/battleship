b52af5f682dc4c528323dfee9835e3b10eae87cc
Patricia Severance <patriciaseverance@gmail.com>
2022-08-30 12:08:51 -0600

    Remove before(:each) method in spec file

--- spec/ship_spec.rb
+++ spec/ship_spec.rb
@@ -1,14 +1,16 @@
 require 'rspec'
 require './lib/ship'
 
-
 describe Ship do
-  before(:each) do
-  cruiser = Ship.new('Cruiser', 3)
-
-  it 'exists and has readable attributes' do
+  it 'exists' do
+    cruiser = Ship.new('Cruiser', 3)
     expect(cruiser).to be_an_instance_of Ship
-    end
   end
-  
+end
+
+describe '#name' do
+  it 'has readable attributes' do
+    cruiser = Ship.new('Cruiser', 3)
+    expect(cruiser.name).to eq 'Cruiser'
+  end
 end
