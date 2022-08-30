541f94214ea9d636c98c5779ac579b885b9ba6c4
Ryan Loughry <thecoderanger5280@icloud.com>
2022-08-30 11:29:22 -0600

    Create cell and the spec file for cell

---
+++ spec/cell_spec.rb
@@ -0,0 +1,13 @@
+require 'rspec'
+#require './lib/ship'
+require './lib/cell'
+
+RSpec.describe Cell do
+    describe '#initialize' do
+        it 'exists' do
+            cell = Cell.new("B4")
+
+            expect(cell).to be_an_instance_of(Cell)
+        end
+    end
+end
