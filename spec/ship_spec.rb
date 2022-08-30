e9cb5c5cd8e181fc5b7ed1937ff20c067506a583
Patricia Severance <patriciaseverance@gmail.com>
2022-08-30 12:15:53 -0600

    Add test case for length method

--- spec/ship_spec.rb
+++ spec/ship_spec.rb
@@ -14,3 +14,10 @@ describe '#name' do
     expect(cruiser.name).to eq 'Cruiser'
   end
 end
+
+describe '#length' do
+  it 'has readable attributes' do
+    cruiser = Ship.new('Cruiser', 3)
+    expect(cruiser.length).to eq 3
+  end
+end
