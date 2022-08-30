53df6d47d58b782bb47cbe42f522380cb4c517e5
Patricia Severance <patriciaseverance@gmail.com>
2022-08-30 11:41:30 -0600

    Add before(:each) method in spec file

--- spec/ship_spec.rb
+++ spec/ship_spec.rb
@@ -1,9 +1,14 @@
 require 'rspec'
 require './lib/ship'
 
+
 describe Ship do
+  before(:each) do
+  cruiser = Ship.new('Cruiser', 3)
+
   it 'exists and has readable attributes' do
-    cruiser = Ship.new('Cruiser', 3)
     expect(cruiser).to be_an_instance_of Ship
+    end
   end
+  
 end
