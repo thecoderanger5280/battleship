421211a71514a12211ed13d4a311ff29a7519465
Patricia Severance <patriciaseverance@gmail.com>
2022-09-02 13:46:12 -0600

    Started placement validation

--- lib/board.rb
+++ lib/board.rb
@@ -14,4 +14,25 @@ class Board
   def valid_coordinate?(coord)
     cells.include?(coord)
   end
+
+  def valid_placement?(ship, coords)
+    x_coords = coords.map{|coord| coord[0]}
+    y_coords = coords.map{|coord| coord[1]}
+    is_valid = !coords.map{|coord| valid_coordinate?(coord)}.include?(false)
+    is_horiz = !x_coords.map{|coord| coord == x_coords[0]}.include?(false)
+    is_vert = !y_coords.map{|coord| coord == y_coords[0]}.include?(false)
+    if ship.length !== coords.length
+      is_valid = false
+    end
+    if is_horiz
+      x_sorted = x_coords.sort
+      i = 0
+      horiz.each do |x|
+
+      end
+
+
+    end
+    y_coords.sort
+  end
 end
