1ad2c71229e3096f4f706495c84bc1ffaae60aeb
Ryan Loughry <thecoderanger5280@icloud.com>
2022-09-02 16:21:11 -0600

    finish valid_placement method

--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -41,7 +41,13 @@ describe '#valid_placement' do
     expect(board.valid_placement?(submarine, ['A1', 'C1'])).to eq(false)
     expect(board.valid_placement?(cruiser, ['A3', 'A2', 'A1'])).to eq(true)
     expect(board.valid_placement?(submarine, ['C1', 'B1'])).to eq(false)
+  end
 
-
+  it 'checks if the coordanites are not diagonal' do
+    board = Board.new
+    cruiser = Ship.new('Cruiser', 3)
+    submarine = Ship.new('Submarine', 2)
+    expect(board.valid_placement?(cruiser, ['A3', 'B2', 'C1'])).to eq(false)
+    expect(board.valid_placement?(submarine, ['C2', 'D3'])).to eq(false)
   end
 end
