cd560846c688d0921233bc8d974231f8ebb74c30
Ryan Loughry <thecoderanger5280@icloud.com>
2022-09-07 15:27:19 -0600

    implement code for placing valid ships for computer turn

--- lib/play.rb
+++ lib/play.rb
@@ -1,5 +1,7 @@
 require './lib/board'
 require './lib/ship'
+require './lib/computer_placement'
+
 class Play
   def start
     p '~*~ Welcome to BATTLESHIP! ~*~'
@@ -19,6 +21,27 @@ class Play
         p "Please input 'p' to play or 'q' to quit"
       end
     end
-    p "#{game_start}"
+    if(game_start)
+      comp_cruiser = Ship.new('Cruiser', 3)
+      comp_submarine = Ship.new('Submarine', 2)
+      comp_cruiser_place = ComputerPlacement.new(comp_cruiser)
+      comp_submarine_place = ComputerPlacement.new(comp_submarine)
+      # p comp_cruiser_place.placement
+      # p comp_submarine_place.placement
+      comp_board = Board.new
+      crusier_coords = comp_cruiser_place.placement
+      comp_board.place(comp_cruiser, crusier_coords)
+      sub_coords = comp_submarine_place.placement
+      # if(comp_board.valid_placement(sub_coords))
+      until(comp_board.valid_placement?(comp_submarine, sub_coords))
+        sub_coords = comp_submarine_place.placement
+      end
+      comp_board.place(comp_submarine,sub_coords)
+      # p sub_coords
+      # p crusier_coords
+    else
+      nil
+    end
+    # p "#{game_start}"
   end
 end
